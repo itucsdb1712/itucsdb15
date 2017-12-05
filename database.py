1782f4f2a83a238df2b2a0babfd3d75fd9599c90
kocaturkog <kocaturkog@itu.edu.tr>
2017-12-05 20:12:05 +0300

    database connection established. database.py created

---
+++ database.py
@@ -0,0 +1,2 @@
+import psycopg2 as dbapi2
+
--- server.py
+++ server.py
@@ -1,18 +1,38 @@
 import datetime
 import os
+import json
+import re
+import psycopg2 as dbapi2
+import database
 
 from flask import Flask
 from flask import render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "verysecretkeyofthewebsite"
 
+def get_elephantsql_dsn(vcap_services):
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={} 
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 @app.route('/')
 def home_page():
-    now = datetime.datetime.now()
-    return render_template('home.html', current_time=now.ctime())
-
+    return render_template('home.html')
+@app.route('/adminpage')
+def admin_page():
+	 return render_template('adminpage.html')
+@app.route('/addcompany')
+def add_company():
+    form = NameForm()
+    return render_template('addcompany.html', form=form)
 
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
@@ -20,4 +40,10 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='postgres' password='itucsdb1712'
+                                        host='localhost' port=5432 dbname='itucsdb'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
